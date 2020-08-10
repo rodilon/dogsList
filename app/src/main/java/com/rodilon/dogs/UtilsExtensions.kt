35907a876841972e5a12522830c89c1a11590e4b
rodilon <renanodilon@gmail.com>
2020-08-10 18:58:29 -0300

    Criação de uma extensão para deixar mais legivel a validação de alguns cenários.

---
+++ app/src/main/java/com/rodilon/dogs/UtilsExtensions.kt
@@ -0,0 +1,26 @@
+package com.rodilon.dogs
+
+import android.app.Activity
+import android.content.Context
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+object UtilsExtensions {
+
+    fun String.isValidEmail(): Boolean {
+        return !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this)
+            .matches()
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    private fun Context.hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
